f401e38a9f8fdfe2a07f74757df72b4e8a76c7e2
gamortegui0515 <gamorteguih@unbosque.edu.co>
2026-08-09 17:49:48 -0500

    Crear portafolio BD2

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://gamortegui0515.github.io',
+  base: '/portafolio-bd2',
+});
